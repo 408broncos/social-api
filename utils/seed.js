a0e51d544c05d2e306e6ce178d9dff99ab77882a
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-05-20 21:50:21 -0700

    fix for loop for seed.js so friendCount is functional

--- utils/seed.js
+++ utils/seed.js
@@ -3,79 +3,87 @@ const { User, Thought, Reaction } = require('../models');
 
 const users = [
     {
-        username: 'Barry',
-        email: 'barry1@gmail.com'
+        username: 'Barry1',
+        email: 'barry11@gmail.com'
     },
     {
-        username: 'Catalina',
-        email: 'catalina1l@gmail.com'
+        username: 'Catalina1',
+        email: 'catalina11@gmail.com'
     },
     {
-        username: 'Doris',
-        email: 'doris1@gmail.com'
+        username: 'Doris1',
+        email: 'doris11@gmail.com'
     },
     {
-        username: 'Eric',
-        email: 'eric1@gmail.com'
+        username: 'Eric1',
+        email: 'eric11@gmail.com'
     },
     {
-        username: 'Favio',
-        email: 'favio1@gmail.com'
+        username: 'Favio1',
+        email: 'favio11@gmail.com'
     },
     {
         username: 'Gary',
-        email: 'gary1@gmail.com'
+        email: 'gary11@gmail.com'
     },
     {
-        username: 'Harry',
-        email: 'harry1@gmail.com'
+        username: 'Harry1',
+        email: 'harry11@gmail.com'
     },
     {
         username: 'Irene',
-        email: 'irene1@gmail.com'
+        email: 'irene11@gmail.com'
     },
     {
-        username: 'Jordan',
-        email: 'jordan1@gmail.com'
+        username: 'Jordan1',
+        email: 'jordan11@gmail.com'
     },
     {
-        username: 'Kate',
-        email: 'kate1@gmail.com'
+        username: 'Kate1',
+        email: 'kate11@gmail.com'
     },
     {
-        username: 'Larry',
-        email: 'larry1@gmail.com'
+        username: 'Larry1',
+        email: 'larry11@gmail.com'
     },
     {
-        username: 'Mary',
-        email: 'mary1@gmail.com'
+        username: 'Mary1',
+        email: 'mary11@gmail.com'
     },
     {
-        username: 'Nate',
-        email: 'nate1@gmail.com'
+        username: 'Nate1',
+        email: 'nate11@gmail.com',
+        thoughtText: 'How many jelly beans are there in the world',
     },
     {
-        username: 'Oscar',
-        email: 'oscar1@gmail.com'
+        username: 'Oscar1',
+        email: 'oscar11@gmail.com'
     },
-  ]
-  
+];
 
-connection.on('error', (err) => err);
+
+connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+});
 
 connection.once('open', async () => {
-    console.log('connected');
-    await Thought.deleteMany({});
-    await User.deleteMany({});
-    await User.insertMany(users);
+    console.log('Connected to MongoDB');
+    
+    try {
+        await Thought.deleteMany({});
+        await User.deleteMany({});
+        const seededUsers = await User.insertMany(users);
 
-    const seededUsers = await User.insertMany(users);
+        for (const user of seededUsers) {
+            const friendCount = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+            await User.findByIdAndUpdate(user._id, { friendCount: friendCount });
+          }
 
-  for (const user of seededUsers) {
-    const friendCount = Math.floor(Math.random() * (10 - 1 + 1) + 1);
-    await User.findByIdAndUpdate(user._id, { friendCount });
-  }
-    console.info('Seeding complete! 🌱');
-    process.exit(0);
-  });
-  
+        console.log('Seeding complete! 🌱');
+    } catch (error) {
+        console.error('Error seeding data:', error);
+    } finally {
+        process.exit(0);
+    }
+});
